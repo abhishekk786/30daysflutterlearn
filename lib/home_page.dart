0340007ba9c26c5de94d434eb89564c138feedf2
abhishekk786 <abhi502ak47@gmail.com>
2021-09-06 12:11:08 +0530

    day 2 done

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+  
+    final int days = 30;
+    final String name = "abhishek kumar";
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+        ),
+      body: Container(
+          child: Center(
+              child: Text("welcome to $days days flutter training by $name")
+              )
+            ),
+    drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext cobntext) {
+   
+    // double pi = 3.14;
+    // bool isMALE = true;
+    // num temp = 30.5;
+
+
+
     return MaterialApp(
-      home: Material(
-        child: Container(
-         child: Center(
-           child: Text("welcome to my app 1 ")
-           ) 
-        ),
-      )
-    );
+        home: HomePage(),
+         );
   }
 }
